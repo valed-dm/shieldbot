bc910d1173185dcfa10eed6be0e7bc999e7f635a
valed-dm <dmvaled@gmail.com>
2024-11-18 12:54:05 +0300

    main.py redundant comments removed

--- bot/main.py
+++ bot/main.py
@@ -15,15 +15,13 @@ from bot.handlers.menu import start_conversation_callback
 load_dotenv()
 API_TOKEN = os.getenv("BOT_TOKEN")
 
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Register handlers
-dp.message.register(menu_command, Command("start"))  # Start command shows the main menu
+dp.message.register(menu_command, Command("start"))
 dp.message.register(message_handler.handle_message)
 dp.callback_query.register(
     start_conversation_callback,
