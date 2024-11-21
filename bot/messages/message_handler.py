9b76777c734c2dd645a519af6b17e2dfa5b73519
valed-dm <dmvaled@gmail.com>
2024-11-21 18:23:24 +0300

    message_handler.py is a template
    for SecureTalk message exchange workflow.

---
+++ bot/messages/message_handler.py
@@ -0,0 +1,21 @@
+from aiogram import types
+
+from bot.keys.symmetric_key import retrieve_symmetric_key
+from bot.messages.encrypt_decrypt import decrypt_message_with_aes
+from bot.messages.encrypt_decrypt import encrypt_message_with_aes
+
+
+async def handle_message(message: types.Message, conversation_id: str):
+    symmetric_key = retrieve_symmetric_key(conversation_id=conversation_id)
+    encrypted_text = await encrypt_message_with_aes(
+        key=symmetric_key,
+        plaintext=message.text,
+    )
+    await message.reply(f"Encrypted: {encrypted_text}")
+
+    # Decrypting the message back (for testing purposes)
+    decrypted_text = await decrypt_message_with_aes(
+        key=symmetric_key,
+        iv_ciphertext=encrypted_text,
+    )
+    await message.reply(f"Decrypted: {decrypted_text}")
