3b03869ab8bf69fd54b9bbccb35b378f630acddc
valed-dm <dmvaled@gmail.com>
2024-11-21 18:01:50 +0300

    The `main.py` file has been refactored
    after the bot instance has been decoupled.

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,10 @@
 import logging
-import os
 
-from aiogram import Bot
 from aiogram import Dispatcher
-from dotenv import load_dotenv
 
-from bot.handlers.callbacks import register_callbacks
-from bot.handlers.messages import register_messages
-
-load_dotenv()
-API_TOKEN = os.getenv("BOT_TOKEN")
-if API_TOKEN is None:
-    msg = "BOT_TOKEN not found! Check your .env file."
-    raise ValueError(msg)
+from bot.callbacks.callbacks import register_callbacks
+from bot.core.bot_instance import bot
+from bot.messages.messages import register_messages
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,7 +13,7 @@ logging.basicConfig(
 
 
 async def main():
-    bot = Bot(token=API_TOKEN)
+    """SecureTalk Bot"""
     dp = Dispatcher()
 
     register_messages(dp)
