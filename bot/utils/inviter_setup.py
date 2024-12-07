36506a841f895fe650cbd85a0a096d4fb94f152b
valed-dm <dmvaled@gmail.com>
2024-12-07 23:35:05 +0300

    inviter_setup.py created

---
+++ bot/utils/inviter_setup.py
@@ -0,0 +1,21 @@
+from uuid import uuid4
+
+import keyring
+
+from bot.core.redis_client import redis_client
+
+
+async def inviter_setup(inviter_id: int, ttl=3600):
+    secure_id = str(uuid4())
+
+    public_key_hex = keyring.get_password("SecureTalk_PublicKey", str(inviter_id))
+    if public_key_hex is None:
+        msg = f"No public key found for inviter ID {inviter_id}."
+        raise ValueError(msg)
+    inviter_public_pem = bytes.fromhex(public_key_hex)
+
+    await redis_client.setex(f"{secure_id}:inviter_id", ttl, inviter_id)
+    await redis_client.setex(f"{secure_id}:inviter_public_key", ttl, inviter_public_pem)
+    await redis_client.setex(f"{secure_id}:conversation_setup", ttl, "in_progress")
+
+    return secure_id
