79b1f5ba98e1e41e993033415d720c2f7b2a933d
valed-dm <dmvaled@gmail.com>
2024-12-09 00:34:46 +0300

    start command processes inviter's and
    invitee's conversation setup operations now.

--- bot/commands/start.py
+++ bot/commands/start.py
@@ -1,15 +1,89 @@
 from __future__ import annotations
 
+import logging
 from typing import TYPE_CHECKING
 
 from bot.keyboards.menu_keyboard import main_menu_keyboard
+from bot.keys.sym_handler import start_close_sym_listener
+from bot.keys.sym_handler import start_sym_handler
+from bot.utils.inviter_workflow import initialize_inviter_workflow
+from bot.utils.resolve_invitation import resolve_invitation
+from bot.utils.store_invitee import store_invitee
 
 if TYPE_CHECKING:
     from aiogram import types
 
+logger = logging.getLogger("START_COMMAND")
 
-async def start_command_menu(message: types.Message):
-    await message.answer(
-        "Welcome to SecureTalk! Choose an action below:",
-        reply_markup=main_menu_keyboard,
-    )
+
+async def start_command(message: types.Message):
+    text = message.text
+
+    # Invitee's side SecureTalkBot operations
+    if len(text.split()) > 1:
+        success = False
+        secure_id = text.split()[1]
+
+        invitee_id = message.from_user.id
+        invitee_username = message.from_user.username  # Username
+        invitee_first_name = message.from_user.first_name  # First Name
+        invitee_last_name = message.from_user.last_name or "not_available"
+
+        msg = f"Resolving invitation: secure_id={secure_id}, invitee_id={invitee_id}"
+        logger.info(msg)
+
+        try:
+            success = await resolve_invitation(secure_id, invitee_id)
+        except Exception as e:
+            msg = f"Invitation has not been resolved for invitee_id={invitee_id}: {e}"
+            logger.exception(msg)
+            await message.answer(
+                f"An error {e} occurred while resolving invitation. Please try again.",
+            )
+
+        if success:
+            await store_invitee(
+                secure_id=secure_id,
+                partner_id=invitee_id,
+                partner_username=invitee_username,
+                partner_first_name=invitee_first_name,
+                partner_last_name=invitee_last_name,
+            )
+
+            await message.answer("You are now connected for secure chat!")
+
+            msg = f"Invitation resolved successfully for invitee_id={invitee_id}"
+            logger.info(msg)
+        else:
+            msg = f"Invalid or expired invitation for invitee_id={invitee_id}"
+            logger.warning(msg)
+            await message.answer("Invalid or expired invitation link.")
+
+    # Inviter's side SecureTalkBot operations
+    else:
+        user_id = message.from_user.id
+
+        try:
+            await initialize_inviter_workflow(inviter_id=user_id)
+            msg = f"RSA key pair for user_id={user_id} created successfully."
+            logger.info(msg)
+        except Exception as e:
+            msg = f"Error initializing RSA keys for user_id={message.from_user.id}: {e}"
+            logger.exception(msg)
+            await message.answer(
+                "An error occurred while setting up your secure chat. "
+                "Please try again.",
+            )
+
+        await message.answer(
+            "Welcome to SecureTalk! Choose an action below:",
+            reply_markup=main_menu_keyboard,
+        )
+
+        close_sym_listener = start_close_sym_listener(inviter_id=user_id)
+        if close_sym_listener:
+            await close_sym_listener
+
+        sym_notification_listener = await start_sym_handler(inviter_id=user_id)
+        if sym_notification_listener:
+            await sym_notification_listener
