ef54010d77c261d19d98f67cc227cbb81b282966
valed-dm <dmvaled@gmail.com>
2024-12-16 02:06:23 +0300

    invitee_deeplink.py refactored for better readability
    with UserDataResolver usage;
    inviter_setup got new arg = inviter.username;

--- bot/messages/invitee_deeplink.py
+++ bot/messages/invitee_deeplink.py
@@ -1,14 +1,25 @@
+import os
+
 from aiogram.types import Message
+from dotenv import load_dotenv
 
 from bot.utils.inviter_setup import inviter_setup
+from bot.utils.user_data_resolver import UserDataResolver
+
+load_dotenv()
+
+LOGO = os.getenv("LOGO")
 
 
 async def invitee_deep_link(message: Message, invitee: str) -> None:
-    inviter_id = message.from_user.id
-    secure_id = await inviter_setup(inviter_id)
+    inviter = UserDataResolver(message)
+
+    secure_id = await inviter_setup(inviter.id, inviter.username)
 
     bot_username = "SecureTalkBot"
     deep_link = f"https://t.me/{bot_username}?start={secure_id}"
-    info = f"SecureTalkBot invitation link for {invitee} is ready:\n{deep_link}"
 
-    await message.answer(text=info)
+    deep_link_text = (
+        f"{LOGO} link for '{invitee}' from '@{inviter.username}':\n{deep_link}"
+    )
+    await message.answer(text=deep_link_text)
