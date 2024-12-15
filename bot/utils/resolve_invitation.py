15f2113449b54e9c516e0b815f8d223a2b24bb9a
valed-dm <dmvaled@gmail.com>
2024-12-16 01:54:37 +0300

    resolve_invitation.py refactored
    to fit new inviter_data type.

--- bot/utils/resolve_invitation.py
+++ bot/utils/resolve_invitation.py
@@ -1,24 +1,31 @@
+from __future__ import annotations
+
 from bot.core.redis_client import get_redis_client
 from bot.utils.conversation_setup import conversation_setup
 
 redis_client = get_redis_client()
 
 
-async def resolve_invitation(secure_id: str, invitee_id: int) -> bool:
-    inviter_id: str | int = await redis_client.get(f"{secure_id}:inviter_id")
-    inviter_public_pem: bytes = await redis_client.get(
-        f"{secure_id}:inviter_public_key",
-    )
+async def resolve_invitation(secure_id: str, invitee_id: int) -> tuple[int, str] | None:
+    inviter_id = None
+    inviter_username = None
+    public_key_hex = None
 
-    if not inviter_id or not inviter_public_pem:
-        return False
+    inviter_data = await redis_client.get(f"{secure_id}:inviter_data")
+    if inviter_data:
+        inviter_id, inviter_username, public_key_hex = inviter_data.split(":")
+
+    if not inviter_id or not public_key_hex:
+        return None
 
     inviter_id = int(inviter_id)
+    inviter_public_pem = bytes.fromhex(public_key_hex)
 
     await conversation_setup(
         inviter_public_pem=inviter_public_pem,
         inviter_id=inviter_id,
+        invitee_id=invitee_id,
         secure_id=secure_id,
     )
 
-    return True
+    return inviter_id, inviter_username
