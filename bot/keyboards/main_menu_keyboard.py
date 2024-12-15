d82192ce4bdef2c03cc4c93582b7ebd57a86efdc
valed-dm <dmvaled@gmail.com>
2024-12-16 02:22:38 +0300

    menu_keyboard.py renamed as
    main_menu_keyboard.py

--- bot/keyboards/main_menu_keyboard.py
+++ bot/keyboards/main_menu_keyboard.py
@@ -1,11 +1,18 @@
+import os
+
 from aiogram.types import InlineKeyboardButton
+from dotenv import load_dotenv
 
 from bot.utils.dynamic_keyboard import dynamic_keyboard
 
+load_dotenv()
+
+LOGO = os.getenv("LOGO")
+
 main_menu_buttons = [
-    InlineKeyboardButton(text="Help", callback_data="help"),
-    InlineKeyboardButton(text="Start SecureTalk", callback_data="start_securetalk"),
+    InlineKeyboardButton(text=LOGO, callback_data="start_securetalk"),
     InlineKeyboardButton(text="Settings", callback_data="settings"),
+    InlineKeyboardButton(text="Help", callback_data="help"),
 ]
 
 main_menu_keyboard = dynamic_keyboard(main_menu_buttons, 3)
