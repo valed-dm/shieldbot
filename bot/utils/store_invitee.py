b1ac9dd84654f16bee5c643ad7830db32b1b2349
valed-dm <dmvaled@gmail.com>
2024-12-16 01:43:18 +0300

    store_invitee.py refactored using UserDataResolver

--- bot/utils/store_invitee.py
+++ bot/utils/store_invitee.py
@@ -1,24 +1,20 @@
 import json
 
 from bot.core.redis_client import get_redis_client
+from bot.utils.user_data_resolver import UserDataResolver
 
 TTL = 3600
 
 redis_client = get_redis_client()
 
 
-async def store_invitee(
-    secure_id: str,
-    partner_id: int,
-    partner_username: str,
-    partner_first_name: str,
-    partner_last_name: str,
-):
+async def store_invitee(secure_id: str, invitee: UserDataResolver):
     invitee_data = {
-        "invitee_id": partner_id,
-        "username": partner_username,
-        "first_name": partner_first_name,
-        "last_name": partner_last_name,
+        "secure_id": secure_id,
+        "invitee_id": invitee.id,
+        "username": invitee.username,
+        "first_name": invitee.first_name,
+        "last_name": invitee.last_name,
     }
     await redis_client.setex(
         f"conversation_invitee:{secure_id}",
