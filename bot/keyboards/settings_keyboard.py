d4ccee11477154b963bc0ca17408e1633ba21ee2
valed-dm <dmvaled@gmail.com>
2024-12-16 02:23:07 +0300

    settings_keyboard.py created

---
+++ bot/keyboards/settings_keyboard.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardButton
+
+from bot.keyboards.button_invite import invite_partner_button
+from bot.utils.dynamic_keyboard import dynamic_keyboard
+
+reset_partners_button = InlineKeyboardButton(
+    text="Reset partners",
+    callback_data="reset_invitees",
+)
+
+settings_menu_buttons = [invite_partner_button, reset_partners_button]
+
+settings_menu_keyboard = dynamic_keyboard(settings_menu_buttons, 3)
