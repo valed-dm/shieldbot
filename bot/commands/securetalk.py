bdc3918f25f7d24572eb02256ae8277f954f55e7
valed-dm <dmvaled@gmail.com>
2024-12-16 02:36:07 +0300

    securetalk.py now checks
    whether there is just a one saved SecureTalk conversation or active conversations are empty.

--- bot/commands/securetalk.py
+++ bot/commands/securetalk.py
@@ -1,13 +1,21 @@
 from aiogram import types
 
-from bot.keyboards.contacts_keyboard import contacts_keyboard
+from bot.keyboards.button_invite import invite_button
+from bot.keyboards.inviter_contacts_keyboard import contacts_keyboard
 
 
 async def on_secure_talk_start(callback_query: types.CallbackQuery):
-    user_contacts = await contacts_keyboard(callback_query.from_user.id)
+    contacts_markup, contacts_qty = await contacts_keyboard(callback_query.from_user.id)
 
-    await callback_query.message.answer(
-        "🔒 Who do you want to initiate SecureTalk with? "
-        "Please share the username or choose from your contacts.",
-        reply_markup=user_contacts,
-    )
+    if contacts_qty != 0:
+        await callback_query.message.answer(
+            "Invite a partner for 🔒SecureTalk from list below;\n"
+            "Use 'Settings'->'Invite partner' if not found in a list.",
+            reply_markup=contacts_markup,
+        )
+    else:
+        invite_partner_keyboard = invite_button()
+        await callback_query.message.answer(
+            "You have not active partners. Invite a partner for 🔒SecureTalk.",
+            reply_markup=invite_partner_keyboard,
+        )
