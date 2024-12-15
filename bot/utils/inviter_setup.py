d52e835bc86ffee40f68dbda428ac3d04f855ab5
valed-dm <dmvaled@gmail.com>
2024-12-16 01:53:06 +0300

    inviter_setup.py got new data representation
    new argument 'inviter_username';
    new inviter_data: f"{inviter_id}:{inviter_username}:{public_key_hex}";

--- bot/utils/inviter_setup.py
+++ bot/utils/inviter_setup.py
@@ -7,17 +7,20 @@ from bot.core.redis_client import get_redis_client
 redis_client = get_redis_client()
 
 
-async def inviter_setup(inviter_id: int, ttl=3600):
+async def inviter_setup(inviter_id: int, inviter_username: str, ttl=3600):
     secure_id = str(uuid4())
 
     public_key_hex = keyring.get_password("SecureTalk_PublicKey", str(inviter_id))
     if public_key_hex is None:
         msg = f"No public key found for inviter ID {inviter_id}."
         raise ValueError(msg)
-    inviter_public_pem = bytes.fromhex(public_key_hex)
 
-    await redis_client.setex(f"{secure_id}:inviter_id", ttl, inviter_id)
-    await redis_client.setex(f"{secure_id}:inviter_public_key", ttl, inviter_public_pem)
+    await redis_client.setex(
+        f"{secure_id}:inviter_data",
+        ttl,
+        f"{inviter_id}:{inviter_username}:{public_key_hex}",
+    )
+
     await redis_client.setex(f"{secure_id}:conversation_setup", ttl, "in_progress")
 
     return secure_id
