11b9829ec9fd363fa23acb281daf3249cefa8759
valed-dm <dmvaled@gmail.com>
2024-12-17 22:02:49 +0300

    main_menu_keyboard.py got minor adjustment
    and uses LOGO env variable.

--- bot/keyboards/main_menu_keyboard.py
+++ bot/keyboards/main_menu_keyboard.py
@@ -10,9 +10,9 @@ load_dotenv()
 LOGO = os.getenv("LOGO")
 
 main_menu_buttons = [
-    InlineKeyboardButton(text=LOGO, callback_data="start_securetalk"),
+    InlineKeyboardButton(text=f"Prepare {LOGO}", callback_data="prepare_securetalk"),
     InlineKeyboardButton(text="Settings", callback_data="settings"),
     InlineKeyboardButton(text="Help", callback_data="help"),
 ]
 
-main_menu_keyboard = dynamic_keyboard(main_menu_buttons, 3)
+main_menu_keyboard = dynamic_keyboard(main_menu_buttons, 2)
