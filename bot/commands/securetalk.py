1d0157e8c5d95564d404bc0da1f91c611f8c632e
valed-dm <dmvaled@gmail.com>
2024-12-17 22:02:07 +0300

    securetalk.py got minor adjustment
    and uses LOGO env variable.

--- bot/commands/securetalk.py
+++ bot/commands/securetalk.py
@@ -1,26 +1,37 @@
+import os
+
 from aiogram import types
+from dotenv import load_dotenv
 
 from bot.core.user_data_resolver import UserDataResolver
 from bot.keyboards.button_invite import invite_button
 from bot.keyboards.inviter_contacts_keyboard import contacts_keyboard
 
+load_dotenv()
 
-async def on_secure_talk_start(callback_query: types.CallbackQuery):
+LOGO = os.getenv("LOGO")
+
+
+async def on_prepare_secure_talk(callback_query: types.CallbackQuery):
     inviter = UserDataResolver(callback_query)
     contacts_markup, contacts_qty = await contacts_keyboard(
         inviter.id,
         inviter.username,
     )
 
+    text_invite = (
+        f"Invite a partner for {LOGO} from list below;\n"
+        f"Use 'Settings:Invite partner' if not found in a list."
+    )
+
     if contacts_qty != 0:
         await callback_query.message.answer(
-            "Invite a partner for 🔒SecureTalk from list below;\n"
-            "Use 'Settings'->'Invite partner' if not found in a list.",
+            text_invite,
             reply_markup=contacts_markup,
         )
     else:
         invite_partner_keyboard = invite_button()
         await callback_query.message.answer(
-            "You have not active partners. Invite a partner for 🔒SecureTalk.",
+            f"No {LOGO} partners were found. Invite a one.",
             reply_markup=invite_partner_keyboard,
         )
