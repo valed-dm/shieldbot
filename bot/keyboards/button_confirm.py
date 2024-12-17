e31bfe4816321cc2ffcce9647f1794ed95420124
valed-dm <dmvaled@gmail.com>
2024-12-17 22:02:26 +0300

    button_confirm.py got minor adjustment
    and uses LOGO env variable.

--- bot/keyboards/button_confirm.py
+++ bot/keyboards/button_confirm.py
@@ -1,10 +1,17 @@
+import os
+
 from aiogram.types import InlineKeyboardButton
 from aiogram.types import InlineKeyboardMarkup
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOGO = os.getenv("LOGO")
 
 
 def confirm_button(securetalk_data: str):
     button = InlineKeyboardButton(
-        text="Accept 🔒SecureTalk",
+        text=f"✅{LOGO}",
         callback_data=securetalk_data,
     )
     return InlineKeyboardMarkup(inline_keyboard=[[button]])
