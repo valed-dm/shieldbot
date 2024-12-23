28b1edead7b838d5f7e80f71f2929d752f8f21a9
valed-dm <dmvaled@gmail.com>
2024-12-23 22:30:24 +0300

    callbacks.py refactored
    to match new callback prefix design (role:action:data);
    got 4 new callbacks: on_reset_invitees, on_manual_invitee_input, on_decrypt_button_click(2 different prefixes for inviter, invitee with partial usage).

--- bot/callbacks/callbacks.py
+++ bot/callbacks/callbacks.py
@@ -1,7 +1,10 @@
+from functools import partial
+
 from aiogram import Router
 from aiogram.filters import Filter
 from aiogram.types import CallbackQuery
 
+from bot.callbacks.decrypt_button_click import on_decrypt_button_click
 from bot.callbacks.invitee.confirm_button_click import on_confirm_button_click
 from bot.callbacks.invitee.invitee_button_input import on_invitee_button_click
 from bot.callbacks.invitee.invitee_manual_input import on_manual_invitee_input
@@ -12,11 +15,13 @@ from bot.commands.settings import on_settings
 
 CALLBACK_HELP = "help"
 CALLBACK_SETTINGS = "settings"
-CALLBACK_PREPARE_SECURETALK = "prepare_securetalk"
-CALLBACK_MANUAL_INVITEE_INPUT = "invite_for_securetalk"
-CALLBACK_SAVED_INVITEE_INPUT = "ir:"
-CALLBACK_ACCEPT_INVITATION = "ie:"
-CALLBACK_RESET_INVITEES = "reset_invitees"
+CALLBACK_PREPARE_SECURETALK = "ir:prepare:"
+CALLBACK_MANUAL_INVITEE_INPUT = "ie:input:"
+CALLBACK_SAVED_INVITEE_INPUT = "ir:invite:"
+CALLBACK_ACCEPT_INVITATION = "ie:accept:"
+CALLBACK_RESET_INVITEES = "ir:reset:"
+CALLBACK_INVITER_DECRYPT = "ir:decrypt:"
+CALLBACK_INVITEE_DECRYPT = "ie:decrypt:"
 
 router = Router(name=__name__)
 
@@ -38,8 +43,16 @@ router.callback_query.register(
     CallbackFilter(CALLBACK_SETTINGS),
 )
 router.callback_query.register(
+    on_reset_invitees,
+    CallbackFilter(CALLBACK_RESET_INVITEES),
+)
+router.callback_query.register(
     on_prepare_secure_talk,
     CallbackFilter(CALLBACK_PREPARE_SECURETALK),
+)
+router.callback_query.register(
+    on_manual_invitee_input,
+    CallbackFilter(CALLBACK_MANUAL_INVITEE_INPUT),
 )
 router.callback_query.register(
     on_invitee_button_click,
@@ -50,10 +63,10 @@ router.callback_query.register(
     lambda c: c.data and c.data.startswith(CALLBACK_ACCEPT_INVITATION),
 )
 router.callback_query.register(
-    on_manual_invitee_input,
-    CallbackFilter(CALLBACK_MANUAL_INVITEE_INPUT),
+    partial(on_decrypt_button_click, expected_prefix="ir:decrypt:"),
+    lambda c: c.data and c.data.startswith(CALLBACK_INVITER_DECRYPT),
 )
 router.callback_query.register(
-    on_reset_invitees,
-    CallbackFilter(CALLBACK_RESET_INVITEES),
+    partial(on_decrypt_button_click, expected_prefix="ie:decrypt:"),
+    lambda c: c.data and c.data.startswith(CALLBACK_INVITEE_DECRYPT),
 )
