0d34497024ff190d4a91ff74a5d6be21e6cabdf3
valed-dm <dmvaled@gmail.com>
2024-12-23 22:23:42 +0300

    button_invite.py refactored
    to match new callback prefix design (role:action:data)

--- bot/keyboards/button_invite.py
+++ bot/keyboards/button_invite.py
@@ -2,8 +2,8 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.types import InlineKeyboardMarkup
 
 invite_partner_button = InlineKeyboardButton(
-    text="Invite partner",
-    callback_data="invite_for_securetalk",
+    text="Invite 🔒",
+    callback_data="ie:input:",
 )
 
 
