f9112d043244a4a41452ec2f281ae96f415f8d2a
valed-dm <dmvaled@gmail.com>
2024-12-23 22:25:37 +0300

    main_menu_keyboard.py refactored
    to match new callback prefix design (role:action:data);
    3 buttons in a row tested;

--- bot/keyboards/main_menu_keyboard.py
+++ bot/keyboards/main_menu_keyboard.py
@@ -10,9 +10,9 @@ load_dotenv()
 LOGO = os.getenv("LOGO")
 
 main_menu_buttons = [
-    InlineKeyboardButton(text=f"Prepare {LOGO}", callback_data="prepare_securetalk"),
+    InlineKeyboardButton(text=f"{LOGO}", callback_data="ir:prepare:"),
     InlineKeyboardButton(text="Settings", callback_data="settings"),
     InlineKeyboardButton(text="Help", callback_data="help"),
 ]
 
-main_menu_keyboard = dynamic_keyboard(main_menu_buttons, 2)
+main_menu_keyboard = dynamic_keyboard(main_menu_buttons, 3)
