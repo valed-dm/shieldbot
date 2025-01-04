c3a6c581046ab71f5c03073a53740a895d20c2f2
valed-dm <dmvaled@gmail.com>
2025-01-04 15:18:10 +0300

    messages.py now imports
    handle_message, on_username_input ann processes '/abort' command

--- bot/messages/messages.py
+++ bot/messages/messages.py
@@ -2,16 +2,18 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.filters import StateFilter
 
+from bot.commands.abort import abort_command
 from bot.commands.start import start_command
 from bot.core.state import UsernameInputState
-from bot.messages.invitee.invitee_text_input import on_invitee_text_input
-from bot.messages.message_handler import handle_message
+from bot.messages.handlers.on_message import handle_message
+from bot.messages.invitee.username_input import on_username_input
 
 router = Router(name=__name__)
 
 router.message.register(start_command, Command("start"))
+router.message.register(abort_command, Command("abort"))
 router.message.register(
-    on_invitee_text_input,
+    on_username_input,
     StateFilter(UsernameInputState.entering_username),
 )
 router.message.register(handle_message)
