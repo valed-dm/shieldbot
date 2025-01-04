f2f07be66b679be3d197786c31d9a0f75cef83a1
valed-dm <dmvaled@gmail.com>
2025-01-05 00:09:33 +0300

    deeplink.py uses SecureTalkData attrs now.

--- bot/messages/handlers/deeplink.py
+++ bot/messages/handlers/deeplink.py
@@ -19,20 +19,23 @@ class DeepLinkHandler(BaseSecureTalkHandler):
             await self.send_notifications()
 
             await self.set_securetalk_state()
-
             return True
+
         return False
 
     async def resolve_deeplink(self):
         """Verify and process a deep link invitation."""
-        self._secure_id = self.text.split()[1]
+        self.secure_talk_data.secure_id = self.text.split()[1]
 
         info_msg = f"{self.logo} Invitation from @{self.sender.username} received."
         logger.info(info_msg)
 
         try:
-            self._inviter_id, self._inviter_username = await resolve_invitation(
-                self._secure_id,
+            (
+                self.secure_talk_data.inviter_id,
+                self.secure_talk_data.inviter_username,
+            ) = await resolve_invitation(
+                self.secure_talk_data.secure_id,
                 self.sender.id,
             )
         except Exception as e:
@@ -46,35 +49,40 @@ class DeepLinkHandler(BaseSecureTalkHandler):
 
     async def store_invitation(self):
         """Store invitee and initialize FSM state."""
-        if not self._inviter_id:
+        if not self.secure_talk_data.inviter_id:
             error_msg = "Inviter ID is missing after invitation resolution."
             raise ValueError(error_msg)
 
-        self._invitee_id = self.sender.id
+        self.secure_talk_data.invitee_id = self.sender.id
 
-        await store_invitee(self._secure_id, self.sender)
+        await store_invitee(self.secure_talk_data.secure_id, self.sender)
 
     async def send_notifications(self):
         """Send notifications to inviter and invitee."""
-        if not self._inviter_id or not self._inviter_username:
+        if (
+            not self.secure_talk_data.inviter_id
+            or not self.secure_talk_data.inviter_username
+        ):
             error_msg = "Incomplete inviter data for notification."
             raise ValueError(error_msg)
 
         contacts, _ = await contacts_keyboard(
-            int(self._inviter_id),
-            self._inviter_username,
+            int(self.secure_talk_data.inviter_id),
+            self.secure_talk_data.inviter_username,
         )
         await self.bot_instance.send_message(
-            self._inviter_id,
+            self.secure_talk_data.inviter_id,
             f"Press button '🔒 {self.sender.username}' to start {self.logo}.",
             reply_markup=contacts,
         )
         await self.bot_instance.send_message(
             self.sender.id,
-            f"Now waiting for {self.logo} with @{self._inviter_username} to start.",
+            f"Now waiting for {self.logo} "
+            f"with @{self.secure_talk_data.inviter_username} to start.",
         )
         info_msg = (
-            f"{self.logo} @{self._inviter_username} invitation resolved successfully: "
+            f"{self.logo} @{self.secure_talk_data.inviter_username} "
+            f"invitation resolved successfully: "
             f"{self.sender.id}:@{self.sender.username}"
         )
         logger.info(info_msg)
