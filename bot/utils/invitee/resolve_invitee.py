2e274eebdd05099cd226a47c62052e3ec1e399af
valed-dm <dmvaled@gmail.com>
2025-01-04 15:19:44 +0300

    resolve_invitee.py imports from deeplink_builder now

--- bot/utils/invitee/resolve_invitee.py
+++ bot/utils/invitee/resolve_invitee.py
@@ -6,7 +6,7 @@ from aiogram.exceptions import TelegramAPIError
 from aiogram.exceptions import TelegramBadRequest
 
 from bot.core.bot_instance import get_bot_instance
-from bot.messages.invitee.invitee_deeplink import invitee_deep_link
+from bot.messages.invitee.deeplink_builder import invitee_deep_link
 from bot.messages.messages_predefined import invalid_format
 from bot.messages.messages_predefined import unexpected_err_msg
 
