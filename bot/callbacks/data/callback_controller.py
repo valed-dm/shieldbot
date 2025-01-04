c8ee429fbabf699032b689e11912e6f5565b894c
valed-dm <dmvaled@gmail.com>
2025-01-05 00:11:23 +0300

    callbacks_controller.py uses SecureTalkData attrs
    and got its own from CallbackData dataclass.

--- bot/callbacks/data/callback_controller.py
+++ bot/callbacks/data/callback_controller.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from bot.callbacks.data.redis_reference import get_callback_data
@@ -14,6 +15,13 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
+@dataclass
+class CallbackData:
+    role: str = None
+    action: str = None
+    reference_id: str = None  # Callback reference id to get data from redis
+
+
 class CallbackController(BaseSecureTalkHandler):
     def __init__(
         self,
@@ -25,9 +33,7 @@ class CallbackController(BaseSecureTalkHandler):
         Initialize the verifier with a callback query and FSM state.
         """
         super().__init__(raw_data, state, bot)
-        self._role = None
-        self._action = None
-        self._reference_id = None  # Callback reference id to get data from redis
+        self.attrs = CallbackData()
 
     async def callback_controller(
         self,
@@ -48,13 +54,15 @@ class CallbackController(BaseSecureTalkHandler):
 
         # Split callback data and extract the reference ID
         try:
-            role_prefix, action, self._reference_id = self.callback_query.data.split(
-                ":",
-                maxsplit=3,
+            role_prefix, action, self.attrs.reference_id = (
+                self.callback_query.data.split(
+                    ":",
+                    maxsplit=3,
+                )
             )
-            self._role = "inviter" if role_prefix == "ir" else "invitee"
-            self._action = action
-            self.callback_data = await get_callback_data(self._reference_id)
+            self.attrs.role = "inviter" if role_prefix == "ir" else "invitee"
+            self.attrs.action = action
+            self.callback_data = await get_callback_data(self.attrs.reference_id)
 
         except ValueError:
             await self.callback_query.answer("❌ Invalid callback structure!")
@@ -70,7 +78,7 @@ class CallbackController(BaseSecureTalkHandler):
             await self.callback_query.answer(msg, show_alert=True)
             return False
 
-        if self._action in ("invite", "accept"):
+        if self.attrs.action in ("invite", "accept"):
             self._extract_conversation_params()
 
         return True
@@ -87,42 +95,42 @@ class CallbackController(BaseSecureTalkHandler):
             invitee_username,
         ) = self.callback_data.split(":")
 
-        comparison_id = inviter_id if self._role == "inviter" else invitee_id
+        comparison_id = inviter_id if self.attrs.role == "inviter" else invitee_id
         if str(self.sender.id) != comparison_id:
             msg = "❌ User ID mismatch in callback data."
             raise ValueError(msg)
 
         # Assign extracted parameters to instance attributes
-        self._secure_id = secure_id
-        self._inviter_id = int(inviter_id)
-        self._inviter_username = inviter_username
-        self._invitee_id = int(invitee_id)
-        self._invitee_username = invitee_username
+        self.secure_talk_data.secure_id = secure_id
+        self.secure_talk_data.inviter_id = int(inviter_id)
+        self.secure_talk_data.inviter_username = inviter_username
+        self.secure_talk_data.invitee_id = int(invitee_id)
+        self.secure_talk_data.invitee_username = invitee_username
 
     @property
     def secure_id(self) -> str:
-        return self._secure_id
+        return self.secure_talk_data.secure_id
 
     @property
     def inviter_id(self) -> int:
-        return self._inviter_id
+        return self.secure_talk_data.inviter_id
 
     @property
     def inviter_username(self) -> str:
-        return self._inviter_username
+        return self.secure_talk_data.inviter_username
 
     @property
     def invitee_id(self) -> int:
-        return self._invitee_id
+        return self.secure_talk_data.invitee_id
 
     @property
     def invitee_username(self) -> str:
-        return self._invitee_username
+        return self.secure_talk_data.invitee_username
 
     @property
     def reference_id(self) -> str:
-        return self._reference_id
+        return self.attrs.reference_id
 
     @property
     def role(self) -> str:
-        return self._role
+        return self.attrs.role
