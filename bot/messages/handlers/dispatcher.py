6e89d4cc721f5c016695255451eac02066ea2d2d
valed-dm <dmvaled@gmail.com>
2025-01-04 22:26:08 +0300

    dispatcher.py class now inherits from BaseSecureTalkHandler.

--- bot/messages/handlers/dispatcher.py
+++ bot/messages/handlers/dispatcher.py
@@ -1,25 +1,25 @@
-from bot.messages.handlers.base import BaseMessageHandler
+from bot.messages.handlers.base import BaseSecureTalkHandler
 
 
-class SecureTalkDispatcherHandler(BaseMessageHandler):
+class SecureTalkDispatcherHandler(BaseSecureTalkHandler):
     async def message_controller(self):
         """Resolves conversation participant's roles."""
         await self.load_securetalk_state()
         # Check if secure conversation is initialized
-        if not self.secure_id:
+        if not self._secure_id:
             await self.message.reply(f"❌{self.logo} is not initialized!")
             return False
 
         # Verify if the sender is part of the secure conversation
-        if self.sender.id not in (int(self.inviter_id), int(self.invitee_id)):
+        if self.sender.id not in (int(self._inviter_id), int(self._invitee_id)):
             await self.message.reply(f"❌You are not part of this {self.logo}!")
             return False
 
         # Determine which side is target now
         target = (
-            (int(self.inviter_id), "inviter", "ir")
-            if self.sender.id == int(self.invitee_id)
-            else (int(self.invitee_id), "invitee", "ie")
+            (int(self._inviter_id), "inviter", "ir")
+            if self.sender.id == int(self._invitee_id)
+            else (int(self._invitee_id), "invitee", "ie")
         )
 
         self.recipient_id, self.recipient_role, self.recipient_prefix = target
