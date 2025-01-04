d2468a1e0db5f2b0f8c36acaaeba49fbd547e9fd
valed-dm <dmvaled@gmail.com>
2025-01-04 22:21:40 +0300

    callback_controller.py class now inherits from BaseMessageHandler refactored.

--- bot/callbacks/data/callback_controller.py
+++ bot/callbacks/data/callback_controller.py
@@ -2,47 +2,38 @@ from __future__ import annotations
 
 import logging
 from typing import TYPE_CHECKING
-from typing import Any
 
 from bot.callbacks.data.redis_reference import get_callback_data
-from bot.core.state import FSMStateManager
-from bot.core.user_data_resolver import UserDataResolver
-from bot.keys.aes.sym_key import retrieve_symmetric_key
-from bot.keys.encrypt_decrypt import decrypt_message_with_aes
+from bot.messages.handlers.base import BaseSecureTalkHandler
 
 if TYPE_CHECKING:
+    from aiogram import Bot
     from aiogram import types
     from aiogram.fsm.context import FSMContext
 
 logger = logging.getLogger(__name__)
 
 
-class CallbackVerifier:
+class CallbackController(BaseSecureTalkHandler):
     def __init__(
         self,
-        callback_query: types.CallbackQuery,
+        raw_data: types.Message | types.CallbackQuery,
         state: FSMContext,
+        bot: Bot,
     ):
         """
         Initialize the verifier with a callback query and FSM state.
         """
-        self.callback_query = callback_query
-        self.state = state
-        self.user_resolver = None  # Will be assigned dynamically
-        self.callback_data = None
-
-        # Conversation info parameters
+        super().__init__(raw_data, state, bot)
         self._role = None
         self._action = None
-        self._secure_id = None
-        self._inviter_id = None
-        self._inviter_username = None
-        self._invitee_id = None
-        self._invitee_username = None
-        # Callback reference id to get data from redis
-        self._reference_id = None
+        self._reference_id = None  # Callback reference id to get data from redis
 
-    async def verify(self, expected_prefix: str, params_count: int) -> bool:
+    async def callback_controller(
+        self,
+        expected_prefix: str,
+        params_count: int,
+    ) -> bool:
         """
         Perform the callback verification process.
 
@@ -63,7 +54,7 @@ class CallbackVerifier:
             )
             self._role = "inviter" if role_prefix == "ir" else "invitee"
             self._action = action
-            self.callback_data = await get_callback_data(self.reference_id)
+            self.callback_data = await get_callback_data(self._reference_id)
 
         except ValueError:
             await self.callback_query.answer("❌ Invalid callback structure!")
@@ -96,10 +87,8 @@ class CallbackVerifier:
             invitee_username,
         ) = self.callback_data.split(":")
 
-        self.user_resolver = UserDataResolver(self.callback_query)
-
         comparison_id = inviter_id if self._role == "inviter" else invitee_id
-        if str(self.user_resolver.id) != comparison_id:
+        if str(self.sender.id) != comparison_id:
             msg = "❌ User ID mismatch in callback data."
             raise ValueError(msg)
 
@@ -109,55 +98,6 @@ class CallbackVerifier:
         self._inviter_username = inviter_username
         self._invitee_id = int(invitee_id)
         self._invitee_username = invitee_username
-
-    async def update_conversation_state(self) -> None:
-        """
-        Update the FSM state with extracted parameters.
-        """
-        fsm_manager = FSMStateManager(self.state)
-        await fsm_manager.load()
-
-        fsm_manager.secure_id = self._secure_id
-        fsm_manager.inviter_id = self._inviter_id
-        fsm_manager.inviter_username = self._inviter_username
-        fsm_manager.invitee_id = self._invitee_id
-        fsm_manager.invitee_username = self._invitee_username
-
-        await fsm_manager.save()
-
-    async def abort_conversation_state(self) -> tuple[str, str]:
-        """
-        Reset the FSM state.
-        """
-        fsm_manager = FSMStateManager(self.state)
-        await fsm_manager.load()
-
-        inviter_username = fsm_manager.inviter_username
-        invitee_username = fsm_manager.invitee_username
-
-        await fsm_manager.clear()
-        await fsm_manager.save()
-
-        return inviter_username, invitee_username
-
-    async def decrypt_text(self) -> tuple[str, Any]:
-        fsm_manager = FSMStateManager(self.state)
-        await fsm_manager.load()
-
-        symmetric_key = retrieve_symmetric_key(conversation_id=fsm_manager.secure_id)
-        iv_ciphertext = bytes.fromhex(self.callback_data)
-
-        decrypted_text = await decrypt_message_with_aes(
-            key=symmetric_key,
-            iv_ciphertext=iv_ciphertext,
-        )
-
-        return (
-            decrypted_text,
-            fsm_manager.inviter_username
-            if self._role == "invitee"
-            else fsm_manager.invitee_username,
-        )
 
     @property
     def secure_id(self) -> str:
