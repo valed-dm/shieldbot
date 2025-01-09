eca59d4e135c341f077afefd7e48826648db6c47
valed-dm <dmvaled@gmail.com>
2025-01-09 13:02:06 +0300

    invitee_manual_input.py got docstrings.

--- bot/callbacks/invitee/invitee_manual_input.py
+++ bot/callbacks/invitee/invitee_manual_input.py
@@ -1,3 +1,5 @@
+"""Module initiates manual invitee's username input"""
+
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 
@@ -8,6 +10,19 @@ async def on_manual_invitee_input(
     callback_query: types.CallbackQuery,
     state: FSMContext,
 ):
+    """
+    Initiates the manual entry of the invitee's username.
+
+    This function transitions the FSM to the `UsernameInputState.entering_username`
+    state and prompts the user to input the invitee's username manually.
+
+    :param callback_query: The callback query that triggered the manual input workflow.
+    :type callback_query: types.CallbackQuery
+    :param state: The current finite state machine (FSM) context.
+    :type state: FSMContext
+
+    :return: None
+    """
     await state.set_state(UsernameInputState.entering_username)
 
     await callback_query.message.answer(
